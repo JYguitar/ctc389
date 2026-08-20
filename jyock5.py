509841500c2565d0d443b23f6ffb4dda98944bcd
JYguitar <JYguitar@yahoo.com>
2026-08-20 01:50:25 +0000

    lab 5 J Yock

---
+++ jyock5.py
@@ -0,0 +1,33 @@
+# CTC389-151 
+# Jeff Yock
+# Lab 5
+
+evens = [22,44,66,88,24,46,68,80,26,48,]
+fruityveg = ["avocado", "mango", "jackfruit", "loquat", "orange"]
+
+# Part 1 
+print ("Evens without Loop")
+
+print (evens[0])
+print (evens[1])
+print (evens[2])
+print (evens[3])
+print (evens[4])
+print (evens[5])
+print (evens[6])
+print (evens[7])
+print (evens[8])
+print (evens[9])
+
+# Part 2 
+print ("Evens with Loop")
+
+for i in evens:
+  print (i)
+
+# Part 3
+print ("Fruityveg with Loop")
+
+for i in fruityveg:
+  print (i)
+
