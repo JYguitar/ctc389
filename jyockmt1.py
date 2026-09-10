962081cf5ebaf507e45dc0868a8c700a410de558
JYguitar <JYguitar@yahoo.com>
2026-09-10 01:01:26 +0000

    ctc389 midterm

---
+++ jyockmt1.py
@@ -0,0 +1,18 @@
+# CTC389-151
+# Jeff Yock
+# Midterm
+# Program 1 
+
+nameIn = input("Hello, please enter your name: ")
+
+numIn = int(input("Please choose and enter any integer number: "))
+
+numAdd = int(input("Please enter a number to add to your choice: "))
+
+newTot = numIn + numAdd
+
+print (nameIn, "you initially chose", numIn, "but added", numAdd)
+
+print ("Your new number is", newTot)
+
+
---
+++ jyockmt2.py
@@ -0,0 +1,32 @@
+# CTC 389-151
+# Jeff Yock
+# Midterm
+# Program 2
+
+def recArea(base,height):
+    recArea = base * height
+    return recArea
+
+print ("This code will calculate the area of a rectangle given base and height.")
+
+basIn = int(input("Please enter the base of your rectangle: "))
+
+hgtIn = int(input("Please enter the height of your rectangle: "))
+
+yourArea = recArea(basIn,hgtIn)
+
+print ("The area of your rectangle is", yourArea, "units.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
